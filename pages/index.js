d935c24d60da9bd263bb82d468b9f42f33e73e70
Fernando Nomellini <fernando@nomellini.net>
2024-05-29 23:52:29 +0000

    Alterando a frase

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,6 @@
 function Home() {
     return <div>
-        <h1>PARWeb</h1>
-        <h2>Atualização em tempo real</h2>
-        <h3>Este é um H3</h3>
-        <h4>Este é um H4</h4>
-        <p>Este é um P</p>
-        <a href='http://www.datamace.com.br'>Tage a</a>
+        <h1>Amo minha familia!</h1>
     </div>
 }
 
