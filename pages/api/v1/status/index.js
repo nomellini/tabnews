ad42a6adb00c38f62e64b6bf5d9acb8619d8cd7a
Fernando Nomellini <fernando@nomellini.net>
2024-06-12 22:59:22 +0000

    .

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,29 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("select 1+1 as soma;");
-  console.log(result.rows);
-  response.status(200).json({ text: "" });
+  const updatedAt = new Date().toISOString();
+
+  const v = await database.query("SHOW server_version;");
+  const database_version = v.rows[0].server_version;
+
+  const mc = await database.query("SHOW max_connections;");
+  const database_max_connections = mc.rows[0].max_connections;
+
+  const dbOpenConnectionResult = await database.query(
+    "select count(1) conexoes from pg_stat_activity where datname = 'local_db'",
+  );
+  const conections = dbOpenConnectionResult.rows[0].conexoes;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: database_version,
+        max_connections: parseInt(database_max_connections),
+        open_connections: parseInt(conections),
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,14 @@
 test("Get to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  console.log(responseBody);
+
+  const parsedUpdatedat = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedat);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toBeDefined();
+  expect(responseBody.dependencies.database.open_connections).toEqual(1);
 });
