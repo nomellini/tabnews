b5c429ef800f6ceac4d775855b12297c7a9841af
Fernando Nomellini <fernando@nomellini.net>
2024-06-14 22:51:14 +0000

    .

--- infra/database.js
+++ infra/database.js
@@ -1,12 +1,22 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
+  console.log({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? "false" : "true",
+  });
+
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
   await client.connect();
   try {
